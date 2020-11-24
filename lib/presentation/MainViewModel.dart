e88a96abec19ff173668dfc1e56607a78af589e0
Sebastian Schneider <sebastian.schneider@simpleclub.com>
2020-11-24 20:11:59 +0100

    Test data

--- lib/presentation/MainViewModel.dart
+++ lib/presentation/MainViewModel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sc_clean_architecture/domain/entities/Wishlist.dart';
 import 'package:sc_clean_architecture/domain/usecases/GetWishlist.dart';
@@ -9,7 +10,14 @@ class MainViewModel extends ChangeNotifier {
   }
 
   Future<void> init() async {
-    wishlist = await GetWishlist(WishlistRepoImpl()).call();
+    // wishlist = await GetWishlist(WishlistRepoImpl()).call();
+    wishlist = Wishlist(<WishlistProduct>[
+      WishlistProduct(3, Icons.ac_unit, 'Test Product', 5.99),
+      WishlistProduct(3, Icons.link, 'Test Link', 15.99),
+      WishlistProduct(3, Icons.padding, 'Test Tom Designer', 0.99),
+      WishlistProduct(3, Icons.airplanemode_inactive_rounded, 'Test Airplane', 599.45),
+      WishlistProduct(3, Icons.wb_cloudy_sharp, 'Test Cloud', 12.99),
+    ]);
     notifyListeners();
   }
 
