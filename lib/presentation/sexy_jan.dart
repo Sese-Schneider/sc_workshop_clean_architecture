582f84a39e69c49f2103d5d749b3d7dd0551e80f
felix <felix.henkemeier@simpleclub.com>
2020-11-24 19:52:52 +0000

    Confetti

--- lib/presentation/sexy_jan.dart
+++ lib/presentation/sexy_jan.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 /// Jan widget that is sexy.
@@ -10,12 +13,46 @@ class SexyJan extends StatefulWidget {
 }
 
 class _SexyJanState extends State<SexyJan> {
+  ConfettiController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ConfettiController()..play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Center(
           child: Image.asset('assets/christmas_jan.png'),
+        ),
+        Positioned(
+          top: -100,
+          left: 0,
+          right: 0,
+          child: ConfettiWidget(
+            confettiController: _controller,
+            blastDirection: pi / 2,
+            blastDirectionality: BlastDirectionality.explosive,
+            emissionFrequency: .1,
+            numberOfParticles: 30,
+            minimumSize: Size(3, 3),
+            maximumSize: Size(9, 9),
+            colors: [
+              Colors.white,
+              Colors.white54,
+              Colors.white70,
+            ],
+            shouldLoop: true,
+          ),
         ),
       ],
     );
