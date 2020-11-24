8cab86934acc529346fb8c9c00477a6b90bef1ad
felix <felix.henkemeier@simpleclub.com>
2020-11-24 19:18:17 +0000

    oop

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/all.dart';
 import 'package:sc_clean_architecture/presentation/MainView.dart';
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sexy Jan's Wishlist",
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(177, 30, 49, 1),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ProviderScope(
+      child: MaterialApp(
+        title: "Sexy Jan's Wishlist",
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(177, 30, 49, 1),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MainView(),
       ),
-      home: MainView(),
     );
   }
 }
